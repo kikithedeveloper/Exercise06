cfad63e9c7df40f118aa813fa00c5af9406688e8
Hackbright Student <info@hackbrightacademy.com>
2014-06-09 16:38:51 -0700

    working version with extra credit

--- wordcount.py
+++ wordcount.py
@@ -7,13 +7,30 @@ text = open(filename)
 wordcount = {}
 
 for line in text:
-    line = line.split(" ")
-    for l in line:
-        if wordcount.get(l):
-            wordcount[l] += 1
+    words = line.strip('\n').split(" ")
+    for w in words:
+        w = w.strip('",.!:;-?_').lower()
+        if "-" in w:
+            clean_word = w.split("-")
+            for c in clean_word:
+                c = c.strip('",.!:;-?_')
+                if wordcount.get(c):
+                    wordcount[c] += 1
+                else:
+                    wordcount[c] = 1
+        elif wordcount.get(w):
+            wordcount[w] += 1
         else:
-            wordcount[l] = 1
+            wordcount[w] = 1
 
-for k, v in wordcount.iteritems():
-    print k, v
+sorted_list = [i for i in wordcount.iteritems()]
+
+sorted_list.sort(key=lambda i: i[0])
+sorted_list.reverse()
+sorted_list.sort(key=lambda i: i[1])
+sorted_list.reverse()
+
+for item in sorted_list:
+    print item[0], item[1]
+
 
