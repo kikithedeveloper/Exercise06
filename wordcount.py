7e845361b63c9c73b38e644f09586cd3bc573cb5
Hackbright Student <info@hackbrightacademy.com>
2014-06-09 17:58:54 -0700

    Working exercise 6 with all extra credit and pretty functions

--- wordcount.py
+++ wordcount.py
@@ -1,36 +1,50 @@
 from sys import argv
 
-script, filename = argv
+def check_count(w, wordcount):
+    w = w.strip('",.!:;-?_()').lower()
+    if wordcount.get(w):
+        wordcount[w] += 1
+    else:
+        wordcount[w] = 1
+    return wordcount
 
-text = open(filename)
+def sort_list(wordcount):
+    sorted_list = [i for i in wordcount.iteritems()]
 
-wordcount = {}
+    sorted_list.sort(key=lambda i: i[0])
+    sorted_list.reverse()
+    sorted_list.sort(key=lambda i: i[1])
+    sorted_list.reverse()
 
-for line in text:
-    words = line.strip('\n').split(" ")
-    for w in words:
-        w = w.strip('",.!:;-?_').lower()
-        if "-" in w:
-            clean_word = w.split("-")
-            for c in clean_word:
-                c = c.strip('",.!:;-?_')
-                if wordcount.get(c):
-                    wordcount[c] += 1
-                else:
-                    wordcount[c] = 1
-        elif wordcount.get(w):
-            wordcount[w] += 1
-        else:
-            wordcount[w] = 1
+    return sorted_list
 
-sorted_list = [i for i in wordcount.iteritems()]
+def main():
+    script, filename = argv
 
-sorted_list.sort(key=lambda i: i[0])
-sorted_list.reverse()
-sorted_list.sort(key=lambda i: i[1])
-sorted_list.reverse()
+    text = open(filename)
 
-for item in sorted_list:
-    print item[0], item[1]
+    wordcount = {}
+
+    for line in text:
+        words = line.strip('\n').split(" ")
+        for word in words:
+            if word.isalpha():
+                wordcount = check_count(word, wordcount)
+            else:
+                for letter in word.lower():
+                    if not letter.isalpha():
+                        clean_word = word.split(letter)
+                        for c in clean_word:
+                            if c != "":
+                                wordcount = check_count(c, wordcount)
+                        break
+
+    sorted_list = sort_list(wordcount)
+
+    for item in sorted_list:
+        print item[0], item[1]
+
+if __name__ == "__main__":
+    main()
 
 
